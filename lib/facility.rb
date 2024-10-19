918f0f03c508493857303667a0d766f27cc99e5a
elysableu <elysabward@gmail.com>
2024-10-18 19:11:08 -0600

    Updated facility: can add vehicle registration to facility.services passed

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,21 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :vehicle_registration
 
   def initialize(facility_details)
     @name = facility_details[:name]
     @address = facility_details[:address]
     @phone = facility_details[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle
+    if @services.include?('Vehicle Registration')
+
+    end
   end
 end
