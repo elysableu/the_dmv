5b38d24a6d78f2dc45b9d0b38dd98b50df99f6b9
elysableu <elysabward@gmail.com>
2024-10-22 17:18:28 -0600

    Refactored facility_builder: Added instance variables to hold facility locations arrays

--- spec/facility_builder_spec.rb
+++ spec/facility_builder_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe FacilityBuilder do
     @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
     @ny_dmv_locations = DmvDataService.new.ny_dmv_office_locations
     @mo_dmv_locations = DmvDataService.new.mo_dmv_office_locations
+    @co_facilities = []
+    @ny_facilities = []
+    @mo_facilities = []
   end
 
   describe '#initialize' do
@@ -16,39 +19,39 @@ RSpec.describe FacilityBuilder do
 
   describe '#build facility' do
     it 'can build an array of new facility objects' do
-      expect(@builder.facilities).to be_empty
-      @builder.build_facility_data(@co_dmv_office_locations)
-      expect(@builder.facilities.length).to eq(@co_dmv_office_locations.length)
-      expect(@builder.facilities[0..2].map(&:name)).to eq(["DMV Tremont Branch", "DMV Northeast Branch", "DMV Northwest Branch"])
+      expect(@co_facilities).to be_empty
+      co_facilities = @builder.build_facility_data(@co_dmv_office_locations)
+      expect(co_facilities.length).to eq(@co_dmv_office_locations.length)
+      expect(co_facilities[0..2].map(&:name)).to eq(["DMV Tremont Branch", "DMV Northeast Branch", "DMV Northwest Branch"])
     end
 
-    xit 'can accept facility data from another data sources' do
-      expect(@builder.facilities).to be_empty
-      @builder.build_facility_data(@ny_dmv_office_locations)
-   
-      expect(@builder.facilities.length).to eq(@ny_dmv_office_locations.length)
-      expect(@builder.facilities[0..2].map(&:name)).to eq(["LAKE PLACID", "HUDSON", "RIVERHEAD KIOSK"])
+    it 'can accept facility data from another data sources' do
+      expect(@ny_facilities).to be_empty
+      puts @ny_dmv_office_locations
+      @ny_facilities = @builder.build_facility_data(@ny_dmv_office_locations)
+      expect(@ny_facilities.length).to eq(@ny_dmv_office_locations.length)
+      expect(@ny_facilities[0..2].map(&:name)).to eq(["LAKE PLACID", "HUDSON", "RIVERHEAD KIOSK"])
     end
 
     xit 'can accept facility data from YET another data source' do
-      expect(@builder.facilities).to be_empty
-      @builder.build_facility_data(@mo_dmv_office_locations)
-      expect(@builder.facilities.length).to eq(@mo_dmv_office_locations.length)
-      expect(@builder.facilities[0..2].map(&:name)).to eq(["FERGUSON-OFFICE CLOSED UNTIL FURTHER NOTICE", "BUTLER", "CENTRAL WENT END"])
+      expect(@mo_facilities).to be_empty
+      @mo_facilities = @builder.build_facility_data(@mo_dmv_office_locations)
+      expect(@mo_facilities.length).to eq(@mo_dmv_office_locations.length)
+      expect(@mo_facilities[0..2].map(&:name)).to eq(["FERGUSON-OFFICE CLOSED UNTIL FURTHER NOTICE", "BUTLER", "CENTRAL WENT END"])
     end
   end
 
   describe '#format_address'do
     it 'can format address' do
-      @builder.build_facility_data(@co_dmv_office_locations)
-      expect(@builder.facilities[0].address).to eq("2855 Tremont Place, Suite 118 Denver, CO 80205")
+      co_facilities = @builder.build_facility_data(@co_dmv_office_locations)
+      expect(co_facilities[0].address).to eq("2855 Tremont Place, Suite 118 Denver, CO 80205")
     end
   end
 
   describe '#format_facility_services' do
     it 'can add services to the new facility object' do
-      @builder.build_facility_data(@co_dmv_office_locations)
-      expect(@builder.facilities[0].services).to eq(["vehicle titles", "registration", "renewals", "VIN inspections"])
+      co_facilities = @builder.build_facility_data(@co_dmv_office_locations)
+      expect(co_facilities[0].services).to eq(["vehicle titles", "registration", "renewals", "VIN inspections"])
     end
   end
 end
