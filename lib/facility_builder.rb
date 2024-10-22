911d42a97fd7cd4bfde6dd06fa4cd6951d5eff15
elysableu <elysabward@gmail.com>
2024-10-22 13:42:36 -0600

    Updated facility_builder: created build_facility_data and added logic to iterate over each entry in dmv_data and return array of facility infomration; passed test can build an array of facility objects

--- lib/facility_builder.rb
+++ lib/facility_builder.rb
@@ -4,4 +4,14 @@ class FacilityBuilder
   def initialize
     @facilities = []
   end
+
+  def build_facility_data(dmv_data)
+    dmv_data.each do |dmv_entry|
+      new_facility = Facility.new({ name: dmv_entry[:dmv_office],
+                      address: "#{dmv_entry[:address_li]} #{dmv_entry[:address_1]} \n #{dmv_entry[:city]} #{dmv_entry[:state]} #{dmv_entry[:zip]}",
+                      phone: dmv_entry[:phone]
+                      })
+      @facilities << new_facility      
+    end
+  end
 end
