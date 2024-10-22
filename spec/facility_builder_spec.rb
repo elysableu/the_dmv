651b7ecd0a8e9cdf276898f36fb0c336735eec96
elysableu <elysabward@gmail.com>
2024-10-22 10:00:06 -0600

    Updated facility_builder_spec: completed writing tests

--- spec/facility_builder_spec.rb
+++ spec/facility_builder_spec.rb
@@ -7,13 +7,16 @@ RSpec.describe FacilityBuilder do
   end
   describe '#initialize' do
     it 'can create new FacilityBuilder object' do
-    
+      expect(@factory).to be_an_instance_of(FacilityBuilder)
     end
   end
 
   describe '#build facility' do
     it 'can build an array of new facility objects' do
-      
+      expect(@builder.facilities).to be_empty
+      @builder.build_facility_data(@co_dmv_office_locations)
+      expect(@builder.facilities.length).to eq(@co_dmv_office_locations.length)
+      expect(@builder.facilities[0..2].map(&:name)).to eq(["DMV Tremont Branch", "DMV Northeast Branch", "DMV Northwest Branch"])
     end
   end
 end
