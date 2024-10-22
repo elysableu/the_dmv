51fcbf98365857987cf382a6de15424e6da78a97
elysableu <elysabward@gmail.com>
2024-10-22 09:50:29 -0600

    Updated facility_builder_spc: added before(:each) block and empty it blocks

--- spec/facility_builder_spec.rb
+++ spec/facility_builder_spec.rb
@@ -2,6 +2,18 @@ require 'spec_helper'
 
 RSpec.describe FacilityBuilder do
   before(:each) do
+    @builder = FacilityBuilder.new
+    @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+  end
+  describe '#initialize' do
+    it 'can create new FacilityBuilder object' do
+    
+    end
+  end
 
+  describe '#build facility' do
+    it 'can build an array of new facility objects' do
+      
+    end
   end
 end
